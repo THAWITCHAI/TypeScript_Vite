0b12853002004f042971ab632fb0f0778b752b5e
Thawitchai <124795506+THAWITCHAI@users.noreply.github.com>
2024-04-18 23:03:16 +0700

    Home Page

---
+++ CRUD/Client/src/App.tsx
@@ -0,0 +1,11 @@
+import Container from 'react-bootstrap/Container';
+import Home from './components/Home';
+function App() {
+  return (
+    <Container>
+      <Home/>
+    </Container>
+  )
+}
+
+export default App
---
+++ CRUD/Client/src/components/Home.tsx
@@ -0,0 +1,10 @@
+
+function Home() {
+  return (
+    <div className="w-25 p-3">
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+export default Home
